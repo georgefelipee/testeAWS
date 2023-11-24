fe8ed5cebeca7d839644acf3bce826a7894480d4
George Felipe <georgefelipe938@gmail.com>
2023-11-24 18:25:13 -0300

    mudança model localizationModel

--- src/models/localizationModel.js
+++ src/models/localizationModel.js
@@ -1,8 +1,8 @@
 import mongoose, { Mongoose } from "mongoose";
 
 const localizationSchema = new mongoose.Schema({
-    latitude: {type: String},
-    longitude: {type: String}
+    latitude: {type: Number},
+    longitude: {type: Number}
 })
 
 const localization = mongoose.model("localizations", localizationSchema);
